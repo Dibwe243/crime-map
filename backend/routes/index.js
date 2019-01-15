d15bbb446fc93f880f7b763059ab0237196827b2
saidaman <saida.sibulali@gmail.com>
2019-01-15 10:11:16 +0200

    delete report under each user

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -19,16 +19,33 @@ route.post('/save-crime-reports', function(req, res){
     myDatabase.create(req.body)
     .then(data => res.send(data))
 })
-route.delete('/delete-reports', function(req, res){
-    
+
+//delete the the account for a user
+route.delete('/delete-account', function(req, res){
     myDatabase.remove()
     .then(data => res.send(data))
     .catch(err => console.log('failed to delete'));
 })
 
+//delete all reports that belong to a user
+route.delete('/delete-reports', function(req, res){
+    var deleteId = req.body.id;
+    myDatabase.update({_id: deleteId}, {$set: {reportCrime :[]}})
+    .then(data => res.send(data))
+    .catch(err => console.log('failed to delete'));
+})
+
+//Delete one report under a specific user
+route.delete('/deleteOneReport', function(req, res){
+    var idToUpdate = req.body.id;
+    var idToDelete = req.body.idToUpdate;
+    myDatabase.update({_id: idToUpdate}, {$pull: {reportCrime: {id: idToDelete}}})
+    .then(data => res.send(data))
+    .catch(err => console.log('failed to delete'));
+})
 route.post('/update-reports', function(req, res){
     
-    myDatabase.create(req.body)
+    myDatabase.update(req.body)
     .then(data => res.send(data))
 })
 
